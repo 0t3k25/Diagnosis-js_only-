0924c7a7d74ac77967359793ff3d6e365911f9bb
hureta <40241920+0t3k25@users.noreply.github.com>
2020-06-19 22:04:04 +0900

    namako

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+if(window.confirm('診断を始めますか？')){
+    window.alert('help:yesまたはnoで答えてください')
+    const ans_01 = window.prompt('おっぱい好き？');
+    if(ans_01 ==="yes"){
+        window.alert('最高か？')
+    } else if(ans_01 ==='no'){
+        window.alert('変態')
+    } else {
+    window.alert('さようなら')
+}
+}
