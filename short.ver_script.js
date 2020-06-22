2ee38d4decdbc33fcefc7d73dd64b8867e1c7e0b
hureta <40241920+0t3k25@users.noreply.github.com>
2020-06-22 11:01:33 +0900

    putting on hold

--- short.ver_script.js
+++ short.ver_script.js
@@ -2,14 +2,11 @@
 //診断開始の質問および返答
 if(window.confirm('診断を始めますか？')){    
     window.alert('help:yesまたはnoで答えてください');
-} else {
-    window.alert('カエレ！');
-}
 //質問内容
 const que = [{toi:'おっぱい好きですか？'},{toi:'同性が好きですか？'},];
 //打ちミスの注意喚起
-const warning = [{note:'yesかnoで答えてください'},{note:'文字は読めますか？'},
-{note:'診断する価値ないですね'}];
+//const warning = [{note:'yesかnoで答えてください'},{note:'文字は読めますか？'},
+//{note:'診断する価値ないですね'}];
 //質問文
 for(let i=0 ; i < que.length ; i++ ){
     const ans = window.prompt(que[i].toi);
@@ -17,6 +14,12 @@ for(let i=0 ; i < que.length ; i++ ){
         window.alert('最高か？')
     } else if(ans ==='no'){
         window.alert('変態')
-    } else {
+    } else if(ans !== "yes" || ans !== "no") {
         window.alert(warning[i].note);
+        ans;
     }}
+    } else {
+    window.alert('カエレ！');
+    }
+
+
